ff08d0539f54a39695a960436e2f15afe1150333
FelipeMonobe <xinube@gmail.com>
2017-05-08 01:01:46 -0300

    moar files

---
+++ Scripts/export_packages.sh
@@ -0,0 +1,3 @@
+pacman -Qqe | grep -Fvx "$(pacman -Qqm)" > ~/.packages/.pacman
+pacman -Qqm > ~/.packages/.aur
+npm list --global --parseable --depth=0 | sed '1d' | awk '{gsub(/\/.*\//,"",$1); print}' > ~/.packages/.npm
